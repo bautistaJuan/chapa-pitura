0368b4984ea2743638befee70c0728d35b810a80
Juan Bautista Vega <jv604824@gmail.com>
2024-08-23 17:24:18 -0300

    Updates

---
+++ index.f68dcc5c.js
@@ -0,0 +1,126 @@
+function e(e){return e&&e.__esModule?e.default:e}var a=globalThis,s={},i={},t=a.parcelRequire4ceb;null==t&&((t=function(e){if(e in s)return s[e].exports;if(e in i){var a=i[e];delete i[e];var t={id:e,exports:{}};return s[e]=t,a.call(t.exports,t,t.exports),t.exports}var r=Error("Cannot find module '"+e+"'");throw r.code="MODULE_NOT_FOUND",r}).register=function(e,a){i[e]=a},a.parcelRequire4ceb=t),(0,t.register)("27Lyk",function(e,a){Object.defineProperty(e.exports,"register",{get:()=>s,set:e=>s=e,enumerable:!0,configurable:!0});var s,i=new Map;s=function(e,a){for(var s=0;s<a.length-1;s+=2)i.set(a[s],{baseUrl:e,path:a[s+1]})}}),t("27Lyk").register(new URL("",import.meta.url).toString(),JSON.parse('["auyDg","index.f68dcc5c.js","b4PbZ","whatsApp.ea70af98.png","579XB","Puliendo-el-auto.c9b7dc5f.jpg","7oAV7","opticas-main.c0227869.png","3DOyq","carroceria.85187558.jpg","knUzj","pintura-services.3da9755f.jpeg","5W3sS","icons8-mercado-pago-144.4b00dc04.png","22ifP","efectivo.a098f6e8.png","lt9uz","car-pintura-services.588d186b.png","5iqZO","car-carroceria-services.eba64965.png","9Sf4w","car-opticas-services.de1b92cd.png"]'));var r={};r=new URL("whatsApp.ea70af98.png",import.meta.url).toString();var n={};n=new URL("Puliendo-el-auto.c9b7dc5f.jpg",import.meta.url).toString();var c={};c=new URL("opticas-main.c0227869.png",import.meta.url).toString();var o={};o=new URL("carroceria.85187558.jpg",import.meta.url).toString();var l={};l=new URL("pintura-services.3da9755f.jpeg",import.meta.url).toString();var p={};p=new URL("icons8-mercado-pago-144.4b00dc04.png",import.meta.url).toString();var d={};d=new URL("efectivo.a098f6e8.png",import.meta.url).toString();var u={};u=new URL("car-pintura-services.588d186b.png",import.meta.url).toString();var m={};m=new URL("car-carroceria-services.eba64965.png",import.meta.url).toString();var v={};v=new URL("car-opticas-services.de1b92cd.png",import.meta.url).toString();const g=document.getElementById("app"),h=[{path:"/error",componente:function(){return document.createElement("div").innerHTML=`<h1 style="color:red">404 NOT FOUND!</h1> <span>P\xe1gina no encontrada, intente con otra.</span>`}},{path:"/",componente:function(){return document.createElement("div").innerHtml=`
+  <section class="section-presentation">
+      <div id="whp" class="continer-whatsapp">
+        <a href="https://wa.me/+5491123344241?text=Hola%20Jos\xe9!" target="_blank" class="whatsapp-a">
+          <img class="whatsapp-img" src="${/*@__PURE__*/e(r)}" alt="WhatsApp" />
+        </a>
+      </div>
+      <h1 class="section-h1">
+        <span class="h1-gradient">chapa</span> <br /><span class="h1-gradient"
+          >y</span
+        ><br />
+        <span class="h1-gradient">pintura</span>
+      </h1>
+      <span class="section-span">j&f</span>
+    </section>
+    <section class="section-services">
+      <h2 class="section-services-h2">Se hacen trabajos de</h2>
+      <div class="section-div-grid">
+        <div class="div-grid-item">
+          <h2 class="div-h2">pulido y abrillantado</h2>
+          <img
+            src=${/*@__PURE__*/e(n)}
+            alt="pulido y abrillantado"
+            class="section-services-img"
+          />
+        </div>
+        <div class="div-grid-item">
+          <h2 class="div-h2">limpieza de opticas</h2>
+          <img
+            src=${/*@__PURE__*/e(c)}
+            alt="Limpieza de opticas"
+            class="section-services-img"
+          />
+        </div>
+      </div>
+      <div class="div-services">
+        <div class="div-services-card">
+          <h2 class="div-h2">Reparacion de carroceria</h2>
+          <img
+            src=${/*@__PURE__*/e(o)}
+            alt="Reparacion de carroceria general"
+            class="section-services-img"
+          />
+        </div>
+        <div class="div-services-card">
+          <h2 class="div-h2">pintura completa</h2>
+          <img
+            src=${/*@__PURE__*/e(l)}
+            alt="Servicio de pintura completa"
+            class="section-services-img"
+          />
+        </div>
+      </div>
+    </section>
+    <h3 class="article-h3">medios de pagos</h3>
+    <article class="article-medio-de-pagos">
+      <div class="medio">
+        <img src=${/*@__PURE__*/e(p)} alt="MP" />
+        <span class="article-span">mercado pago</span>
+      </div>
+      <div class="medio">
+        <img src=${/*@__PURE__*/e(d)} alt="Efectivo" />
+        <span class="article-span">Efectivo</span>
+      </div>
+  </article>
+`}},{path:"/services",componente:function(){return document.createElement("div").innerHTML=`
+    <h1 class="h1-services" >Servicios</h1>
+    <section class="section"> 
+    
+     <div id="whp" class="continer-whatsapp">
+        <a href="https://wa.me/+5491123344241?text=Hola%20Jos\xe9!" target="_blank" class="whatsapp-a">
+          <img class="whatsapp-img" src="${/*@__PURE__*/e(r)}" alt="WhatsApp" />
+        </a>
+      </div>
+      <div class="card pintura">
+        <img src=${/*@__PURE__*/e(u)} alt="img-card" />
+        <ul class="ul">
+          <li class="ul-li">Brillo y protecci\xf3n. Repintado completo para tu auto</li>
+          <li class="ul-li">
+            \xbfRayones y golpes? \xa1Los solucionamos! Retoques profesionales para tu
+            auto.
+          </li>
+        </ul>
+      </div>
+      <div class="card opticas">
+        <img src=${/*@__PURE__*/e(v)} alt="img-card" />
+        <ul class="ul">
+          <li class="ul-li">Faros como nuevos. Pulido profesional de \xf3pticas.</li>
+          <li class="ul-li">Eliminaci\xf3n rayones y opacidad.</li>
+        </ul>
+      </div>
+      <div class="card carroceria">
+        <img src=${/*@__PURE__*/e(m)} alt="img-card" />
+        <ul class="ul">
+          <li class="ul-li">
+            Eliminamos abolladuras sin necesidad de pintar. Reparaci\xf3n de
+            abolladuras
+          </li>
+          <li class="ul-li">Soluci\xf3n r\xe1pida y eficaz.</li>
+        </ul>
+      </div>
+    </section>
+  `}},{path:"/works",componente:function(){return document.createElement("div").innerHTML=`
+   <div class="works">
+    <div class="works-div">
+    <h3 class="h3-services" >Actualmente solo muestro mis trabajos a trav\xe9s de mis redes sociales, te invito a visitar mi p\xe1gina de 
+    <span>
+     <a class="a-facebook" href="https://www.facebook.com/jose.vega0512/photos" alt="Facebook">
+     Facebook
+     </a>
+     </span>
+     </h3>
+     <span>
+     \xa1Tambi\xe9n estoy subiendo fotos y videos de mis trabajos a mis estados de 
+     <span>
+      <a class="a-whatsapp" href="https://wa.me/+5491123344241?text=Hola%20Jos\xe9!" alt="Whatsapp">
+        Whatsapp
+      </a>
+      </span> 
+      todo el tiempo, mandame un mesaje para que te pueda agendar y puedas verlos!
+    </span>
+    </div>
+   </div>
+
+  `}}];function f(e){try{let a=h.find(a=>a.path==e)||h.find(e=>"/error"==e.path);if(!a){console.log("Page Not Found!");return}let s=a.componente();return g.innerHTML=" ",g.innerHTML=s}catch(e){console.log("Error loading page:",e)}}!function(){let e=document.querySelector("#menu-window");document.addEventListener("click",a=>{let s=a.target;if(s.matches(".a-menu")){a.preventDefault();let i=new URL(s.href).pathname;window.history.pushState({},"",i),e.style.display="none",f(i);return}return s.matches("#open-button")?(a.preventDefault(),e.style.display="flex"):s.matches("#close-button")?(a.preventDefault(),e.style.display="none"):void console.log("Auch!")}),window.addEventListener("load",()=>f(location.pathname)),window.addEventListener("popstate",()=>f(location.pathname))}();
+//# sourceMappingURL=index.f68dcc5c.js.map
